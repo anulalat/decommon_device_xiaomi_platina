b0bf0bca9f601b03b4a82929cdb8a2517fe7e33e
Hanif Ardhani <hanif.ardhani98@gmail.com>
2020-05-26 00:10:06 +0000

    platina: Add device maintainer prop

--- bliss_platina.mk
+++ bliss_platina.mk
@@ -48,6 +48,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Build type
 BLISS_BUILDTYPE=OFFICIAL
 
+# Device Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bliss.maintainer=Hanif Ardhani
+
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
