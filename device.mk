05283a166a8dd720797d22a661c6eb819a0b6975
anulalat <ndra.bodoh@gmail.com>
2020-08-13 00:24:38 +0000

    platina: remove ims boot jar

--- device.mk
+++ device.mk
@@ -244,10 +244,6 @@ PRODUCT_PACKAGES += \
     ims-ext-common_system \
     ims_ext_common.xml
 
-# IMS JAR
-PRODUCT_BOOT_JARS += \
-    ims-ext-common_system
-
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
