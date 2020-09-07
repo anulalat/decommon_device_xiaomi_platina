bb3b2baf7b2bf09a9d2fd00b31677b7894c03a1d
anulalat <ndra.bodoh@gmail.com>
2020-09-07 02:09:46 +0000

    platina: Add target vendor device name

---
+++ cartel_platina.mk
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/platina/device.mk)
+
+# Inherit some common Bliss ROMS stuff.
+$(call inherit-product, vendor/cartel/config/common.mk)
+
+# Inherit from Qualcomm Common Tree
+$(call inherit-product device/qcom/common/common.mk)
+
+# Inherit AOSP product configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="platina" \
+    TARGET_DEVICE="platina"
+
+TARGET_VENDOR_PRODUCT_NAME := platina
+TARGET_VENDOR_DEVICE_NAME := platina
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build type
+CARTEL_BUILD_TYPE := OFFICIAL
+
+# Device identifier
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := cartel_platina
+PRODUCT_DEVICE := platina
+PRODUCT_MODEL := MI 8 Lite
